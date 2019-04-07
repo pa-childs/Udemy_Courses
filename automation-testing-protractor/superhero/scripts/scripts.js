5530de34f2b7858a9dc75f2e1eb6064c0bb179cd
Paul Childs <pachilds@gmail.com>
2019-04-07 09:08:14 -0400

    Add script to add Hero to the Roster.

--- automation-testing-protractor/superhero/scripts/scripts.js
+++ automation-testing-protractor/superhero/scripts/scripts.js
@@ -1,6 +1,16 @@
 $( "#addHero-form" ).submit(function( event ) {
-  $("#hero-list").append('<li class="list-group-item">' + $("#heroInput").val() + '</li>')
+  $("#addHero-Alert").addClass("hide")
 
+  if ($("#heroInput").val() == '') {
+
+    $("#addHero-Alert").removeClass("hide")
+    $("#addHero-Alert").text("You must enter a Hero.")
+
+  } else {
+
+    $("#hero-list").append('<li class="list-group-item">' + $("#heroInput").val() + '</li>')
+    $("#heroInput").val("")
+  }
 
   event.preventDefault();
 });
