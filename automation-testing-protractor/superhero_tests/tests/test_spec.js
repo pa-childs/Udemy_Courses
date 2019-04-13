c4d834f6e44d448096013221a7c96f5d99a5b91c
Paul Childs <paul.childs@clarivate.com>
2019-04-13 15:08:30 -0400

    Add in beforeEach and afterEach functions.

--- automation-testing-protractor/superhero_tests/tests/test_spec.js
+++ automation-testing-protractor/superhero_tests/tests/test_spec.js
@@ -1,5 +1,7 @@
 describe('Super Hero Page', function() {
-	it('should load the correct URL', function() {
+
+	// To do before each test case
+	beforeEach(function() {
 
 		// Only for non-Angular sites
 		browser.ignoreSynchronization = true    
@@ -7,14 +9,28 @@ describe('Super Hero Page', function() {
 		// Load the URL to the browser
 		browser.get('file:///C:/Work_Files/Repositories/Udemy_Courses/automation-testing-protractor/superhero/index.html')
 
+	})
+
+	// To do after each test case
+	afterEach(function() {
+
+
+
+	})
+
+
+	it('should load the correct URL', function() {
+
+
+
 		// Enter login credentials
 		element(by.id('loginEmail')).sendKeys('stan_lee@marvel.com')
 		element(by.id('loginPassword')).sendKeys('spiderman')
 
 		// Verify elements
 		expect(element(by.id('loginEmail')).getAttribute('value')).toEqual('stan_lee@marvel.com')  // Text in field
-		expect(element(by.id('login-title')).getText()).toEqual('Welcome, Please log in.')         // Text on page
-		expect(element(by.id('login-title')).getText()).toContain('Welcome')			           // Text on page
+		expect(element(by.id('login-title')).getText()).toEqual('Welcome, Please log in.')         // Text on page (Whole String)
+		expect(element(by.id('login-title')).getText()).toContain('Welcome')			           // Text on page (Substring)
 
 		// browser.sleep(5000)
 	})
