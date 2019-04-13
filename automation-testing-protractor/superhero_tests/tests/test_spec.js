f3d3cef73db24bf17b1148287dade709876ca6b3
Paul Childs <paul.childs@clarivate.com>
2019-04-13 14:52:24 -0400

    Add some elements to fill in.

--- automation-testing-protractor/superhero_tests/tests/test_spec.js
+++ automation-testing-protractor/superhero_tests/tests/test_spec.js
@@ -2,6 +2,10 @@ describe('Super Hero Page', function() {
 	it('should load the correct URL', function() {
 		browser.ignoreSynchronization = true    //Only for non-Angular sites
 		browser.get('file:///C:/Work_Files/Repositories/Udemy_Courses/automation-testing-protractor/superhero/index.html')
+
+		// Enter login credentials
+		element(by.id('loginEmail')).sendKeys('stan_lee@marvel.com')
+		element(by.id('loginPassword')).sendKeys('spiderman')
 		browser.sleep(5000)
 	})
 })
