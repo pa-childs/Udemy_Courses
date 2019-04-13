96c0202dc74b8219c7fe76a101421b3342c543d0
Paul Childs <paul.childs@clarivate.com>
2019-04-13 15:03:06 -0400

    Verify some elements on the login page.

--- automation-testing-protractor/superhero_tests/tests/test_spec.js
+++ automation-testing-protractor/superhero_tests/tests/test_spec.js
@@ -1,11 +1,21 @@
 describe('Super Hero Page', function() {
 	it('should load the correct URL', function() {
-		browser.ignoreSynchronization = true    //Only for non-Angular sites
+
+		// Only for non-Angular sites
+		browser.ignoreSynchronization = true    
+		
+		// Load the URL to the browser
 		browser.get('file:///C:/Work_Files/Repositories/Udemy_Courses/automation-testing-protractor/superhero/index.html')
 
 		// Enter login credentials
 		element(by.id('loginEmail')).sendKeys('stan_lee@marvel.com')
 		element(by.id('loginPassword')).sendKeys('spiderman')
-		browser.sleep(5000)
+
+		// Verify elements
+		expect(element(by.id('loginEmail')).getAttribute('value')).toEqual('stan_lee@marvel.com')  // Text in field
+		expect(element(by.id('login-title')).getText()).toEqual('Welcome, Please log in.')         // Text on page
+		expect(element(by.id('login-title')).getText()).toContain('Welcome')			           // Text on page
+
+		// browser.sleep(5000)
 	})
 })
