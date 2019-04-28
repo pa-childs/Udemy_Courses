10cb6664cae41ff753812c5c572f3e0d127f033d
Paul Childs <paul.childs@clarivate.com>
2019-04-28 11:31:47 -0400

    Improve Jasmine console reporting by adding jasmine-spec-reporter

--- automation-testing-protractor/superhero_tests/config.js
+++ automation-testing-protractor/superhero_tests/config.js
@@ -1,3 +1,5 @@
+let SpecReporter = require('jasmine-spec-reporter').SpecReporter;
+
 exports.config = {
 	
 	framework: 'jasmine2',
@@ -5,19 +7,49 @@ exports.config = {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
 
 	capabilities: {
+
 		browserName: 'chrome'
+
 	},
+
+	jasmineNodeOpts: {
+
+		showColors: true,
+	    silent: true,
+	    defaultTimeoutInterval: 360000,
+	    print: function () {
+
+	    }
+
+	},
+
+	logLevel: 'WARN',
+  	onPrepare: function () {
+
+	    jasmine.getEnv().addReporter(new SpecReporter({
+
+	      spec: {
+
+	      	displayStacktrace: true
+
+	      },
+
+	      summary: {
+
+	      	displayDuration: false
+
+	      }
+
+	    }));
+
+	},	
 
 	specs: [
 		
 		'./tests/login_spec.js',
 		'./tests/navbar_spec.js',
 		'./tests/home_spec.js'
-	],
+	
+	]
 
-	jasmineNodeOpts: {
-
-		showColors: true
-
-	}
 }
