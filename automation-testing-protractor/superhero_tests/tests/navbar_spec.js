605aa3d80dcc60631219a9bd9c005a07afb8c699
Paul Childs <paul.childs@clarivate.com>
2019-04-28 11:10:54 -0400

    Finish Logout test cases.

--- automation-testing-protractor/superhero_tests/tests/navbar_spec.js
+++ automation-testing-protractor/superhero_tests/tests/navbar_spec.js
@@ -1,25 +1,25 @@
 // NavBar Tests
 
 //Page Object Files
-var NavBar = require('../page_objects/navbar.js')
-var LoginPage = require('../page_objects/login_page.js')
+var NavBar = require('../page_objects/navbar.js');
+var LoginPage = require('../page_objects/login_page.js');
 
 describe('NavBar', function() {
 
-	var navBar
-	var loginPage
+	var navBar;
+	var loginPage;
 
 	beforeEach(function() {
 
-		navBar = new NavBar()
-		loginPage = new LoginPage()
+		navBar = new NavBar();
+		loginPage = new LoginPage();
 
 		// Need for non-Angular testing
-		browser.ignoreSynchronization = true
+		browser.ignoreSynchronization = true;
 		browser.driver.manage().window().maximize();
 
 		// Open browser and load URL
-		browser.get('file:///C:/Work_Files/Repositories/Udemy_Courses/automation-testing-protractor/superhero/index.html')
+		browser.get('file:///C:/Work_Files/Repositories/Udemy_Courses/automation-testing-protractor/superhero/index.html');
 
 	})
 
@@ -29,22 +29,31 @@ describe('NavBar', function() {
 	})
 
 	// Logout menu option
-	fit('should display Login page without email and password', function() {
+	it('should display Login page without email and password', function() {
 
-		loginPage.emailField.sendKeys('stan_lee@marvel.com')
-		loginPage.passwordField.sendKeys('spiderman')
-		loginPage.submitButton.click()
+		loginPage.emailField.sendKeys('stan_lee@marvel.com');
+		loginPage.passwordField.sendKeys('spiderman');
+		loginPage.submitButton.click();
 
-		navBar.logoutLink.click()
+		navBar.logoutLink.click();
 
-		expect(loginPage.passwordField.getAttribute('value')).toEqual('')
-		expect(loginPage.emailField.getAttribute('value')).toEqual('')
+		expect(loginPage.passwordField.getAttribute('value')).toEqual('');
+		expect(loginPage.emailField.getAttribute('value')).toEqual('');
 
 	})
 
-	it('should display Login page without email and password', function() {
+	fit('should display Login page with email and password', function() {
 
+		loginPage.emailField.sendKeys('stan_lee@marvel.com');
+		loginPage.passwordField.sendKeys('spiderman');
+		loginPage.rememberMeCheckbox.click();
+		loginPage.submitButton.click();
 
+		navBar.logoutLink.click();
+
+		expect(loginPage.emailField.getAttribute('value')).toEqual('stan_lee@marvel.com');
+		expect(loginPage.passwordField.getAttribute('value')).toEqual('spiderman');
+		expect(loginPage.rememberMeCheckbox.isSelected()).toBe(false);
 		
 	})
 
